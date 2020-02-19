69c9ce5ecd866750153bb963990a7e71a20e8ddd
kevinbpedersen1 <61202791+kevinbpedersen1@users.noreply.github.com>
2020-02-19 14:41:22 +0100

    Initial commit

---
+++ src/main/java/ArrayUtils.java
@@ -0,0 +1,11 @@
+public class ArrayUtils {
+
+    public static String arrayToString(int[]a){
+        String s = "{";
+        for (int i=0;i<a.length; i++){
+            s+= " " + a[i];
+        }
+        s+="{";
+        return s;
+    }
+}
